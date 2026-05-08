767de62671520c1b7dc1c3d20a62ecbc8501adc4
Maynardo-Cornelius <maynardo.cornelius@gmail.com>
2026-05-08 22:42:32 +0700

    Delete src directory

--- src/payment/payment.factory.ts
+++
@@ -1,9 +0,0 @@
-import { Injectable } from '@nestjs/common';
-import { PaymentInterface, QRISPayment } from './payment.implementations';
-
-@Injectable()
-export class PaymentFactory {
-  createPayment(): PaymentInterface {
-    return new QRISPayment();
-  }
-}
