b466be9c659b88020f703d027d1da825339f3a34
Maynardo-Cornelius <maynardo.cornelius@gmail.com>
2026-05-12 11:03:15 +0700

    connect supabase

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,26 +1,28 @@
-import { Module } from '@nestjs/common';
-import { TypeOrmModule } from '@nestjs/typeorm';
-import { AuthModule } from './auth/auth.module';
-import { UsersModule } from './users/users.module';
-import { TenantsModule } from './tenants/tenants.module';
-import { MenuModule } from './menu/menu.module';
-import { CartModule } from './cart/cart.module';
-import { OrderModule } from './order/order.module';
-import { PaymentModule } from './payment/payment.module';
-import { NotificationModule } from './notification/notification.module';
+import { Module } from "@nestjs/common";
+import { TypeOrmModule } from "@nestjs/typeorm";
+import { AuthModule } from "./auth/auth.module";
+import { UsersModule } from "./users/users.module";
+import { TenantsModule } from "./tenants/tenants.module";
+import { MenuModule } from "./menu/menu.module";
+import { CartModule } from "./cart/cart.module";
+import { OrderModule } from "./order/order.module";
+import { PaymentModule } from "./payment/payment.module";
+import { NotificationModule } from "./notification/notification.module";
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
   imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
     TypeOrmModule.forRoot({
-      type: 'mysql',
-      host: 'localhost',
-      port: 3306,
-      username: 'root',
-      password: '',
-      database: 'beebites',
-      entities: [__dirname + '/**/*.entity{.ts,.js}'],
+      type: "postgres",
+      host: process.env.DB_HOST,
+      port: parseInt(process.env.DB_PORT),
+      username: process.env.DB_USERNAME,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_DATABASE,
+      entities: [__dirname + "/**/*.entity{.ts,.js}"],
       synchronize: true,
-      logging: true,
+      ssl: { rejectUnauthorized: false },
     }),
     AuthModule,
     UsersModule,
