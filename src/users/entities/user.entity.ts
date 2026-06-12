08da9cea9e556653bc0bb70d313301ce2904cb29
Vergeo <vergeovalenting@gmail.com>
2026-06-12 17:58:39 +0700

    add tenant role

--- src/users/entities/user.entity.ts
+++ src/users/entities/user.entity.ts
@@ -1,37 +1,44 @@
-import { Entity, PrimaryGeneratedColumn, Column, CreateDateColumn, UpdateDateColumn } from 'typeorm';
-import { Exclude } from 'class-transformer';
-import { ApiProperty } from '@nestjs/swagger';
+import {
+	Entity,
+	PrimaryGeneratedColumn,
+	Column,
+	CreateDateColumn,
+	UpdateDateColumn,
+} from "typeorm";
+import { Exclude } from "class-transformer";
+import { ApiProperty } from "@nestjs/swagger";
 
 export enum UserRole {
-  CUSTOMER = 'customer',
-  ADMIN = 'admin',
+	CUSTOMER = "customer",
+	ADMIN = "admin",
+	TENANT = "tenant",
 }
 
-@Entity('users')
+@Entity("users")
 export class User {
-  @ApiProperty()
-  @PrimaryGeneratedColumn()
-  userId: number;
+	@ApiProperty()
+	@PrimaryGeneratedColumn()
+	userId: number;
 
-  @ApiProperty()
-  @Column({ type: 'varchar', length: 500 })
-  name: string;
+	@ApiProperty()
+	@Column({ type: "varchar", length: 500 })
+	name: string;
 
-  @ApiProperty()
-  @Column({ type: 'varchar', length: 500, unique: true })
-  email: string;
+	@ApiProperty()
+	@Column({ type: "varchar", length: 500, unique: true })
+	email: string;
 
-  @Exclude()
-  @Column({ type: 'varchar', length: 500 })
-  password: string;
+	@Exclude()
+	@Column({ type: "varchar", length: 500 })
+	password: string;
 
-  @ApiProperty()
-  @Column({ type: 'varchar', length: 500, default: UserRole.CUSTOMER })
-  role: string;
+	@ApiProperty()
+	@Column({ type: "varchar", length: 500, default: UserRole.CUSTOMER })
+	role: string;
 
-  @CreateDateColumn()
-  createdAt: Date;
+	@CreateDateColumn()
+	createdAt: Date;
 
-  @UpdateDateColumn()
-  updatedAt: Date;
+	@UpdateDateColumn()
+	updatedAt: Date;
 }
